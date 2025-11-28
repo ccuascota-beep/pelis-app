95df445cccca1fcf555eebd47c7b44993abfe219
ccuascota-beep <carloscuascota2007@gmail.com>
2025-11-27 20:09:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Fragment} from 'react'
+import './App.css'
+import {peliculas} from '../data/peliculas.json'
+function App() {
+
+  return (
+    <Fragment>
+        <peliculas/>
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/assets/componentes/peliculas.jsx
@@ -0,0 +1,41 @@
+import {useEffect, useState} from "react";
+
+export default function peliculas() {
+    const [peliculas, setPeliculas] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const traerImagenPeliculas = async () => {
+
+        try {
+            const result = await fetch(`https://imdb.iamidiotareyoutoo.com/search?q=Spiderman`)
+            const data = await result.json()
+
+
+            const listaPeliculas = data.description;
+            const promesasPeliculas = listaPeliculas.map ( async (pelicula) => {
+                const img = await (data.description["#IMG_POSTER"])
+                return {
+                    ...pelicula,
+                    imagen: img
+                }
+            })
+
+    const peliculas = async () => {
+        try {
+            const results = await fetch("https://imdb.iamidiotareyoutoo.com/search?q=Spiderman")
+            const data = await results.json()
+
+            setPeliculas(data.results)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    useEffect(() => {
+        peliculas()
+    }, []);
+
+    return (
+        <div className=""></div>
+    )
+ }
+
